b0f8c3e0028d197fc2ded97a8b6914b3f29f9b2a
code with J3 <jatinjangel27@gmail.com>
2026-03-21 16:27:36 +0530

    Update User.java

--- backend/src/main/java/com/student/registration/student_registration_backend/model/User.java
+++ backend/src/main/java/com/student/registration/student_registration_backend/model/User.java
@@ -1,24 +1,32 @@
 package com.student.registration.student_registration_backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Data // This annotation from Lombok automatically generates getters, setters, and other methods.
+@Table(name = "users")
+@Data
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
     private String course;
+
+    @Column(name = "student_class")
     private String studentClass;
+
     private Double percentage;
+
     private String branch;
+
+    @Column(name = "mobile_number")
     private String mobileNumber;
 }
